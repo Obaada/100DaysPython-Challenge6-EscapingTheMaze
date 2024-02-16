eca3d9299fbb1b73f20a36a1128b153cd5747b96
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-16 01:41:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Maze code with simple solution 
+# def turn_right():
+    #   turn_left()
+    #   turn_left()
+    #   turn_left()
+    
+    # while not at_goal():
+    #   if right_is_clear():
+    #       turn_right()
+    #       move()
+    #   elif front_is_clear():
+    #       move()
+    #   else:
+    #       turn_left() 
+    ##################################################################
+# Maze code with solving a conditon where 4 right turns might lead to an infinate loop
+    # TESTNUMBER = 0
+    # def turn_right():
+    #     turn_left()
+    #     turn_left()
+    #     turn_left()
+    
+    # while not at_goal():
+    #     if right_is_clear() and TESTNUMBER < 4:
+    #         turn_right()
+    #         move()
+    #         TESTNUMBER += 1
+    #     elif front_is_clear():
+    #         move()
+    #         TESTNUMBER = 0
+    #     else:
+    #         turn_left() 
+    
+    
